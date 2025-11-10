abee2d2a14a224408e362e5b3e9d9e2517601481
misganawtad <misganawt@gmail.com>
2025-11-10 09:17:15 +0900

    Update AppInit.java

--- src/main/java/web/config/AppInit.java
+++ src/main/java/web/config/AppInit.java
@@ -3,13 +3,21 @@ package web.config;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
-    @Override protected Class<?>[] getRootConfigClasses() {
-        return new Class<?>[]{ JpaConfig.class }; // <— add this
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+      
+        return new Class<?>[]{ RootConfig.class };
     }
-    @Override protected Class<?>[] getServletConfigClasses() {
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+       
         return new Class<?>[]{ WebConfig.class };
     }
-    @Override protected String[] getServletMappings() {
+
+    @Override
+    protected String[] getServletMappings() {
         return new String[]{ "/" };
     }
 }
