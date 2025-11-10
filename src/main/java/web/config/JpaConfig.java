e20317b9f2d3641aaed155292cc882a6fed3bbc1
misganawtad <misganawt@gmail.com>
2025-11-10 09:17:48 +0900

    Update JpaConfig.java

--- src/main/java/web/config/JpaConfig.java
+++ src/main/java/web/config/JpaConfig.java
@@ -7,13 +7,11 @@ import org.springframework.dao.annotation.PersistenceExceptionTranslationPostPro
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
-@EnableTransactionManagement
 public class JpaConfig {
 
     @Bean
