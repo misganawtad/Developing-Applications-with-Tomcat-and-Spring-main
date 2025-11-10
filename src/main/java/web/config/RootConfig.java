403d7d122429a6bf6b85b8b8bf504ec731f1a786
misganawtad <misganawt@gmail.com>
2025-11-10 09:22:45 +0900

    Update RootConfig.java

--- src/main/java/web/config/RootConfig.java
+++ src/main/java/web/config/RootConfig.java
@@ -1,1 +1,17 @@
 
+package web.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan(basePackages = {
+        "web.service",
+        "web.repository"
+})
+@Import(JpaConfig.class)
+public class RootConfig {
+}
